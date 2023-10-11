3e7a25162be88d3c49c5bd5a6a4a83d7ed0e8dec
reeft137 <reeft137@gmail.com>
2023-10-11 14:59:37 +0900

    clang-format style google

--- lua/lsp-cmp-config/formatter.lua
+++ lua/lsp-cmp-config/formatter.lua
@@ -3,10 +3,25 @@ if not status then
   vim.notify("formatter not found")
   return
 end
+local util = require "formatter.util"
 
 formatter.setup {
   filetype = {
     sh = require("formatter.filetypes.sh").shfmt,
+    cpp = {
+      function()
+        return {
+          exe = "clang-format",
+          args = {
+            "-assume-filename",
+            util.escape_path(util.get_current_buffer_file_name()),
+            "-style=google",
+          },
+          stdin = true,
+          try_node_modules = true,
+        }
+      end
+    },
     python = require("formatter.filetypes.python").black,
     rust = require("formatter.filetypes.rust").rustfmt,
   },
